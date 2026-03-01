0bef5c5a364945919e153de6083617f580659213
Ndambia <brianndambia6@gmail.com>
2026-03-01 17:52:58 +0300

    Neurolab New

---
+++ Visualizer/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "neurolab-pro-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./engine.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
